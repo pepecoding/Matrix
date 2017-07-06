0e9fc520c8a44f5d6a4efb7ee79aa44444132121
student <student@K0304.step.lan>
2017-07-06 20:59:21 +0300

    latest

--- workspace/WorkWithFiles/test/org/itstep/LogFileCreatorTest.java
+++ workspace/WorkWithFiles/test/org/itstep/LogFileCreatorTest.java
@@ -3,6 +3,9 @@ package org.itstep;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.junit.AfterClass;
@@ -14,54 +17,68 @@ public class LogFileCreatorTest {
 	public static void tearDownAfterClass() throws Exception {
 		File file = new File("D:\\test\\testFileLog.txt");
 		file.delete();
-		
-		
-		
-		
-		
+
 	}
+
 	@Test
 	public void testGetLogListIfSizeOne() {
 		LogFileCreator fileCreator = new LogFileCreator();
 		ArrayList<LogForExample> logs = fileCreator.getLogList(1);
 		assertEquals(1, logs.size());
-		
+
 	}
+
 	@Test
 	public void testGetLogListIfSizeNegative() {
 		LogFileCreator fileCreator = new LogFileCreator();
 		ArrayList<LogForExample> logs = fileCreator.getLogList(-1);
-		assertTrue(logs.isEmpty());	
-						
-		}
-			
+		assertTrue(logs.isEmpty());
+
+	}
+
 	@Test
 	public void testGetLogListIfSizeZero() {
 		LogFileCreator fileCreator = new LogFileCreator();
 		ArrayList<LogForExample> logs = fileCreator.getLogList(0);
 		assertTrue(logs.isEmpty());
-				
+
 	}
+
 	@Test
 	public void testGetLogListIfSizeMoreThanOne() {
 		LogFileCreator fileCreator = new LogFileCreator();
 		ArrayList<LogForExample> logs = fileCreator.getLogList(15);
 		System.out.println(Integer.MAX_VALUE);
 		assertEquals(15, logs.size());
-		
-		
+
 	}
-		@Test
+
+	@Test
 	public void testCreateFile() {
-		
-		
+		LogFileCreator fileCreator = new LogFileCreator();
+		ArrayList<LogForExample> logs = fileCreator.getLogList(15);
+		String filePath = "D:\\test\\testFileLog.txt";
+		fileCreator.createFile(logs, filePath);
+		boolean fileExist = false;
+		try {
+			FileReader reader = new FileReader(filePath);
+			fileExist = true;
+			reader.close();
+		} catch (FileNotFoundException e) {
+			fileExist = false;
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		assertTrue(fileExist);
 	}
-		@Test
-	public void testGetRandomNumber(){
-		
+
+	@Test
+	public void testGetRandomNumber() {
+		LogFileCreator fileCreator = new LogFileCreator();
+		long rndNumber = fileCreator.getRandomNumber(10, 20);
+		assertTrue(rndNumber >= 10 && rndNumber <= 20);
 	}
-		
-		
-	}
-	
-	
+
+}
